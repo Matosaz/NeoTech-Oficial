f575c35a1b3e2c1e817c629f4acf55904d412cb0
Matosaz <mathusphd20@gmail.com>
2024-06-03 17:33:21 -0300

    Neotech-Oficial

---
+++ login-cadastro/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginCadastro from './Components/LoginCadastro/LoginCadastro';
+
+function App() {
+  return (
+    <div>
+      <LoginCadastro/>
+    </div>
+  );
+}
+
+export default App;
---
+++ login-cadastro/src/Components/LoginCadastro/LoginCadastro.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import './LoginCadastro.css' 
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+
+const LoginCadastro = () => {
+
+  const [action, setAction] = useState("Cadastrar");
+
+  return (
+    <div className='container'>
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+
+      <div className="inputs">
+        {action==="Login"?<div></div>:<div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="Nome"/>
+        </div>}
+        
+
+        <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" placeholder="Email"  />
+        </div>
+
+        <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" placeholder="Senha" />
+        </div>
+        {action==="Cadastrar"?<div></div>: <div className="senhaesquecida">Esqueceu a senha?</div>}
+        <div className="submit-container">
+          <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Cadastrar")}}>Cadastrar</div>
+          <div className={action==="Cadastrar"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}><a href="http://127.0.0.1:5500/Inicial/index.html">Login</a></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginCadastro
